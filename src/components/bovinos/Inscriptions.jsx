7b8f1ea66996bd8fe2005fe736f3be9ae22637ec
Bryan <bryansanudo95@gmail.com>
2024-02-01 21:16:33 -0500

    inscripciones

--- src/components/bovinos/Inscriptions.jsx
+++ src/components/bovinos/Inscriptions.jsx
@@ -9,10 +9,11 @@ import { motion } from "framer-motion";
 import { staggerContainer, zoomIn, fadeIn } from "@/utils/motion";
 
 import Metodology from "@/components/bovinos/Metodology";
+import bovinos from "@/assets/courses/curso-6.png";
 
 const Inscriptions = ({ darkMode }) => {
   return (
-    <div
+    /*  <div
       className={` bg-gray-100 w-[20%] flex-col items-center fixed right-0 top-32 md:flex hidden z-30 justify-center  shadow-md  rounded-xl pb-6 ml-auto mr-5   ${
         darkMode ? "shadow-white" : "shadow-black"
       }`}
@@ -82,6 +83,29 @@ const Inscriptions = ({ darkMode }) => {
           </Link>
         </div>
       </motion.div>
+    </div> */
+    <div className="card bg-base-100   shadow-md shadow-primary fixed right-8 top-32 z-30">
+      <figure className="relative">
+        <img src={bovinos} className="h-[400px] w-[400px]" alt="Shoes" />
+        {/* <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+          <Link to="">
+            <button className="btn btn-primary">Más Información</button>
+          </Link>
+        </div> */}
+      </figure>
+      <div className="flex flex-col items-center rounded-b-2xl justify-center py-8">
+        <h2 className="card-title">Bovinos en la clínica diaria</h2>
+
+        <div className="flex items-center justify-center gap-4">
+          <CountDown />
+        </div>
+        <span className=" font-bold mt-4  ">$1.000.000</span>
+        <Link to="/contact">
+          <button className={`${styles.button} bg-primary  `}>
+            Inscripciones
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
