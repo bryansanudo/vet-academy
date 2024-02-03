01187f0d96b704c0a22d6528714c5caf063a3528
Bryan <bryansanudo95@gmail.com>
2024-02-03 12:10:59 -0500

    refactor masterclass

--- src/components/masterclass/Catalogue.jsx
+++ src/components/masterclass/Catalogue.jsx
@@ -1,18 +1,14 @@
 import styles from "@/style";
 
-import { Link } from "react-router-dom";
-import curso from "@/assets/curso.jpg";
 import logo from "@/assets/vet-academy-logo.jpg";
-import { CiSearch } from "react-icons/ci";
 
-import { useEffect, useState } from "react";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { useEffect } from "react";
 
 import curso1 from "@/assets/masterclass/gratuito1.png";
 import curso2 from "@/assets/masterclass/gratuito2.png";
 
 const Catalogue = ({ darkMode }) => {
-  const feed = [
+  const masterclass = [
     {
       id: 1,
       img: curso1,
@@ -34,28 +30,9 @@ const Catalogue = ({ darkMode }) => {
     },
   ];
 
-  const itemsPerPage = 8;
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = feed.slice(indexOfFirstItem, indexOfLastItem);
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  const nextPage = () => {
-    if (currentPage < Math.ceil(feed.length / itemsPerPage)) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const prevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
   useEffect(() => {
     window.scrollTo(0, 150);
-  }, [currentPage]);
+  }, []);
   return (
     <section
       id="clients"
@@ -71,22 +48,9 @@ const Catalogue = ({ darkMode }) => {
           <br className="sm:block hidden" />
         </h2>
       </div>
-      <div className="max-w-md shadow-black shadow-md rounded-xl mb-6">
-        <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-          <div className="grid place-items-center h-full w-12 text-gray-300">
-            <CiSearch />
-          </div>
 
-          <input
-            className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
-            type="text"
-            id="search"
-            placeholder="Buscar..."
-          />
-        </div>
-      </div>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1  w-full gap-10 mb-16  ">
-        {currentItems.map(
+        {masterclass.map(
           ({ id, img, logo, title, subtitle, price, link, italic }) => (
             <div
               key={id}
@@ -105,55 +69,10 @@ const Catalogue = ({ darkMode }) => {
               <div className="card-body">
                 <h2 className={` ${styles.title} ${italic} `}>{title}</h2>
                 <p className={`${styles.subtitle}`}>{subtitle}</p>
-                <div className="card-actions justify-end">
-                  {/* <div>
-                    <span className="text-primary  font-bold">${price}</span>
-                  </div> */}
-                </div>
               </div>
             </div>
           )
         )}
-      </div>
-      <div className="hidden md:flex  items-center w-full justify-center gap-4 ">
-        <button className="" onClick={prevPage}>
-          <FaArrowLeft className="dark:text-white" />
-        </button>
-
-        {Array.from({
-          length: Math.ceil(feed.length / itemsPerPage),
-        }).map((_, index) => (
-          <div
-            className={`cursor-pointer rounded-full h-8 w-8 flex items-center justify-center ${
-              currentPage === index + 1 ? "bg-primary" : "bg-gray-600"
-            }  `}
-            key={index}
-            onClick={() => paginate(index + 1)}
-          >
-            <button className={` text-white`}>{index + 1}</button>
-          </div>
-        ))}
-
-        <button className="" onClick={nextPage}>
-          <FaArrowRight className="dark:text-white" />
-        </button>
-      </div>
-
-      <div className="flex items-center justify-start w-full gap-2 md:hidden">
-        <p className={`${styles.title}`}>Pagina actual</p>
-        <select
-          value={currentPage}
-          onChange={(e) => paginate(parseInt(e.target.value, 10))}
-          className="dark:bg-gray-800 dark:text-white px-2 py-1 border rounded"
-        >
-          {Array.from({
-            length: Math.ceil(feed.length / itemsPerPage),
-          }).map((_, index) => (
-            <option key={index} value={index + 1}>
-              {index + 1}
-            </option>
-          ))}
-        </select>
       </div>
     </section>
   );
--- src/components/masterclass/Masterclass.jsx
+++ src/components/masterclass/Masterclass.jsx
@@ -11,12 +11,6 @@ const Masterclass = ({ darkMode }) => {
   }, []);
   return (
     <>
-      {/*   <div className={`dark:bg-accent  ${styles.flexStart} `}>
-        <div className={`${styles.boxWidth}`}>
-          <HeroAbout darkMode={darkMode} />
-        </div>
-      </div> */}
-
       <div
         className={`dark:bg-accent  ${styles.paddingX} ${styles.flexCenter} mt-20`}
       >
@@ -25,14 +19,9 @@ const Masterclass = ({ darkMode }) => {
             className={`
                pink__gradient absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full `}
           />
-          {/* <Ally /> */}
 
           <Catalogue darkMode={darkMode} />
 
-          {/*      <Services darkMode={darkMode} /> */}
-
-          {/*  <Timeline darkMode={darkMode} /> */}
-          {/*  <Clients /> */}
           <CTA darkMode={darkMode} />
         </div>
       </div>
