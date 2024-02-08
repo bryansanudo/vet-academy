50d78ef1515d34a0b4abdad952962debef4729a1
Bryan <bryansanudo95@gmail.com>
2024-02-08 12:35:32 -0500

    courses

--- src/components/Admin/UserProfilePanel.jsx
+++ src/components/Admin/UserProfilePanel.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-
 import { db } from "@/configFirebase";
-
 import { toast } from "react-toastify";
 import { doc, setDoc } from "firebase/firestore";
+
+const availableCourses = [
+  "bovinos",
+  "manejo del paciente",
+  "infecciones",
+  "enfermedades",
+];
 
 const UserProfilePanel = ({ userData, onReturn }) => {
   const initialState = {
@@ -15,21 +20,31 @@ const UserProfilePanel = ({ userData, onReturn }) => {
 
   const [updateUser, setUpdateUser] = useState({ ...initialState });
 
-  const handleInputChange = (e) => {
-    setUpdateUser({ ...updateUser, [e.target.name]: e.target.value });
-    console.log(updateUser);
+  const handleCourseChange = (course) => {
+    const isChecked = updateUser.courses.includes(course);
+    if (isChecked) {
+      setUpdateUser((prevState) => ({
+        ...prevState,
+        courses: prevState.courses.filter((item) => item !== course),
+      }));
+    } else {
+      setUpdateUser((prevState) => ({
+        ...prevState,
+        courses: [...prevState.courses, course],
+      }));
+    }
   };
 
   const handleReturn = () => {
     onReturn();
   };
 
-  const editUser = (e) => {
+  const editUser = async (e) => {
     e.preventDefault();
 
     try {
-      setDoc(doc(db, "users", initialState.email), { ...updateUser });
-      toast.success("Usuario actualizado con exito");
+      await setDoc(doc(db, "users", initialState.email), { ...updateUser });
+      toast.success("Usuario actualizado con éxito");
     } catch (error) {
       toast.error(error.message);
     }
@@ -37,14 +52,14 @@ const UserProfilePanel = ({ userData, onReturn }) => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center mx-auto pt-10 ">
-        <h2 className="font-bold text-4xl text-center text-transparent bg-clip-text  bg-gradient-to-r from-[#ffcdc2] to-[#6057ca] hover:from-[#6057ca] hover:to-[#ffcdc2] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 md:text-5xl p-2">
+      <div className="flex flex-col items-center justify-center mx-auto pt-10">
+        <h2 className="font-bold text-4xl text-center text-transparent bg-clip-text bg-gradient-to-r from-[#ffcdc2] to-[#6057ca] hover:from-[#6057ca] hover:to-[#ffcdc2] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 md:text-5xl p-2">
           Panel de Usuario
         </h2>
         <h2 onClick={handleReturn} className="cursor-pointer">
           Volver
         </h2>
-        <div className="flex flex-col gap-6 w-[300px] lg:w-[500px] items-center shadow-lg shadow-gray-500 rounded-xl p-8 mt-16 ">
+        <div className="flex flex-col gap-6 w-[300px] lg:w-[500px] items-center shadow-lg shadow-gray-500 rounded-xl p-8 mt-16">
           <p>Correo: {updateUser.email}</p>
           <p>Nombre: {updateUser.name}</p>
           <p>Titulo: {updateUser.title}</p>
@@ -53,7 +68,7 @@ const UserProfilePanel = ({ userData, onReturn }) => {
       </div>
       <div className="bg-green-500">
         <form onSubmit={editUser}>
-          <label className="w-full text-left">Nombre Del usuario</label>
+          <label className="w-full text-left">Nombre del usuario</label>
           <input
             required
             type="text"
@@ -62,8 +77,7 @@ const UserProfilePanel = ({ userData, onReturn }) => {
             value={updateUser.name}
             onChange={(e) => handleInputChange(e)}
           />
-          <label className="w-full text-left">titulo</label>
-
+          <label className="w-full text-left">Titulo</label>
           <input
             required
             type="text"
@@ -72,7 +86,20 @@ const UserProfilePanel = ({ userData, onReturn }) => {
             value={updateUser.title}
             onChange={(e) => handleInputChange(e)}
           />
-          <button>actualizar usuario</button>
+          <label className="w-full text-left">Cursos</label>
+          {availableCourses.map((course) => (
+            <div key={course}>
+              <input
+                type="checkbox"
+                id={course}
+                name={course}
+                checked={updateUser.courses.includes(course)}
+                onChange={() => handleCourseChange(course)}
+              />
+              <label htmlFor={course}>{course}</label>
+            </div>
+          ))}
+          <button>Actualizar Usuario</button>
         </form>
       </div>
     </>
