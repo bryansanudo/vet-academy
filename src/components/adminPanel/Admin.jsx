eb1c8bba1baf750171747ca739c51fb806d9ea48
Bryan <bryansanudo95@gmail.com>
2024-02-09 12:50:01 -0500

    all user

--- src/components/adminPanel/Admin.jsx
+++ src/components/adminPanel/Admin.jsx
@@ -5,23 +5,39 @@ import SearchOrCreate from "@/components/adminPanel/SearchOrCreate";
 import EditUser from "@/components/adminPanel/EditUser";
 import AllUsers from "@/components/adminPanel/AllUsers";
 import { NavLink } from "react-router-dom";
+import { GrPowerShutdown } from "react-icons/gr";
+import styles from "@/style";
+import { FaUsers } from "react-icons/fa";
+import { PiUsersThreeLight } from "react-icons/pi";
+import { BsPersonAdd } from "react-icons/bs";
 
 const Admin = () => {
   return (
-    <div className="flex flex-col items-center absolute w-full   bg-yellow-500   z-50 ">
-      <div className=" bg-red-500 h-32 fixed flex items-center justify-between w-full md:px-24 px-6   ">
-        <NavLink to="enrollment-user">
-          <div>Matricular Usuario</div>
-        </NavLink>
-        <NavLink to="all-users">
-          <div>Todos los Inscritos</div>
-        </NavLink>
+    <div className="flex flex-col items-center absolute w-full   bg-gray-100   z-50 ">
+      <div className=" bg-gray-400 h-20   fixed flex items-center justify-between z-50 w-full md:px-24 px-6  ">
         <NavLink to="/">
-          <div>Salir Admin Panel</div>
+          <div className="flex flex-col items-center justify-center hover:scale-110 duration-500 text-black ">
+            <GrPowerShutdown className="text-2xl" />
+            Salir
+          </div>
+        </NavLink>
+        <div className="bg-white h-10 w-[1px]"></div>
+        <NavLink to="all-users">
+          <div className="flex flex-col items-center justify-center hover:scale-110 duration-500 text-black">
+            <PiUsersThreeLight className="text-2xl" />
+            Inscritos
+          </div>
+        </NavLink>
+        <div className="bg-white h-10 w-[1px]"></div>
+        <NavLink to="enrollment-user">
+          <div className="flex flex-col items-center justify-center hover:scale-110 duration-500 text-black ">
+            <BsPersonAdd className="text-2xl" />
+            Matricular
+          </div>
         </NavLink>
       </div>
       <div className="w-full md:px-20 px-6   ">
-        <div className="w-full bg-green-500 mt-40 mb-20  ">
+        <div className="w-full  mt-40 mb-20  ">
           <Routes>
             <Route path="register-user" element={<RegisterUser />} />
             <Route path="enrollment-user" element={<SearchOrCreate />} />
--- src/components/adminPanel/AllUsers.jsx
+++ src/components/adminPanel/AllUsers.jsx
@@ -17,6 +17,9 @@ import Notiflix from "notiflix";
 import { useDispatch, useSelector } from "react-redux";
 import { STORE_USERS, selectUsers } from "@/redux/slice/userSlice";
 import useFetchCollection from "@/customHooks/useFetchCollection";
+import { FaUserDoctor } from "react-icons/fa6";
+import { GoDot } from "react-icons/go";
+import styles from "@/style";
 
 const AllUsers = () => {
   const { data, isLoading } = useFetchCollection("users");
@@ -70,25 +73,52 @@ const AllUsers = () => {
         <p>No users found.</p>
       ) : (
         <>
-          <h2 className="bg-red-500 flex items-center justify-center">
-            titulo
-          </h2>
-          <div className="grid grid-cols-3 gap-20">
+          <h2 className="flex items-center  justify-center mb-6">titulo</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-20">
             {users.map((user, index) => {
               const { email, name, title, courses } = user;
+
               return (
-                <div key={email} className=" border-black bg-blue-500    ">
-                  <div>{email}</div>
-                  <div>{name}</div>
-                  <div>{title}</div>
-                  <p>Cursos: {courses.join(", ")}</p>
-                  <Link to={`/admin/edit-user/${email}`}>
-                    <FaEdit className="text-2xl text-green-500" />
-                  </Link>
-                  <FaTrashAlt
-                    onClick={() => confirmDelete(email)}
-                    className="cursor-pointer text-2xl text-red-500"
-                  />
+                <div
+                  key={email}
+                  className="flex flex-col items-center justify-between shadow-sm shadow-black rounded-xl  pt-4   "
+                >
+                  <div className="flex items-center justify-center gap-4">
+                    <div className="">{email}</div>
+                    <div className="flex items-center justify-center p-2 bg-primary  mask mask-circle ">
+                      <FaUserDoctor className="text-2xl text-white" />
+                    </div>
+                  </div>
+                  <div className=" flex items-center justify-center flex-col mt-4">
+                    <div className="text-lg">{name}</div>
+                    {/* <div className="  text-md ">{title}</div> */}
+                  </div>
+
+                  <div className="shadow-sm shadow-gray-400 p-1 rounded-lg my-4">
+                    <div>
+                      {courses.map((course, i) => (
+                        <div key={i} className="flex items-center">
+                          <GoDot />
+                          <div>{course}</div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-center gap-3 py-4">
+                    <Link to={`/admin/edit-user/${email}`}>
+                      <div className={`${styles.button} gap-2`}>
+                        Editar
+                        <FaEdit className="text-2xl " />
+                      </div>
+                    </Link>
+                    <div
+                      onClick={() => confirmDelete(email)}
+                      className={`${styles.button} gap-2 cursor-pointer`}
+                    >
+                      Borrar
+                      <FaTrashAlt className="cursor-pointer text-2xl " />
+                    </div>
+                  </div>
                 </div>
               );
             })}
